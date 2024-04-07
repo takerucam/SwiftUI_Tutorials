0f832a5b41062bab00f262cdf19f858742a48778
Takeru Miyake <m18kecam777@gmail.com>
2024-04-08 00:29:16 +0900

    ✨ chapter2クリア

--- SwiftUI_Tutorials/ContentView.swift
+++ SwiftUI_Tutorials/ContentView.swift
@@ -9,16 +9,64 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            DayForecast(
+                day: "Mon",
+                isRainy: false,
+                high: 70,
+                low: 50
+            )
+            
+            DayForecast(
+                day: "Tue",
+                isRainy: true,
+                high: 60,
+                low: 40
+            )
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct DayForecast: View {
+    let day: String
+    let isRainy: Bool
+    let high: Int
+    let low: Int
+    
+    var iconName: String {
+        if isRainy {
+            return "cloud.rain.fill"
+        } else {
+            return "sun.max.fill"
+        }
+    }
+    
+    var iconColor: Color {
+        if isRainy {
+            return Color.blue
+        } else {
+            return Color.yellow
+        }
+    }
+    
+    var body: some View {
+        VStack {
+            Text(day)
+                .font(.headline)
+            Image(systemName: iconName)
+                .foregroundColor(iconColor)
+                .font(.largeTitle)
+                .padding(5)
+            Text("Height: \(high)")
+                .fontWeight(.semibold)
+            Text("Low: \(low)")
+                .fontWeight(.medium)
+                .foregroundStyle(.secondary)
+        }
+        .padding()
+    }
 }
